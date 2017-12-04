e5e34900f40f7caed2028fadd17d1f6de9981632
marcelraas <marcelraas@godatadriven.com>
2017-12-04 21:25:01 +0100

    Add doc strings, parametrize tuning

--- music_generator/musical/notes.py
+++ music_generator/musical/notes.py
@@ -1,5 +1,17 @@
+"""Module for notes and frequencies
+
+Definitions:
+    * Symbol: the note's symbolic name (e.g. C, F#)
+
+Conventions:
+    * In a sequence of notes, octaves increment at each following C, i.e. A#2, B2, C3, C#3, etc.
+    * Only sharp symbols are allowed in note symbolic notation, i.e. use C# not Db
+    * Symbols are written in upper case
+"""
+
 BASE_NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 _A4_BASE = 57
+_A4_TUNING = 440
 
 
 class Note(object):
@@ -7,23 +19,34 @@ class Note(object):
         self.semi_from_c0 = self.calc_semi_from_c0(symbol, octave)
 
     def get_octave(self):
+        """Returns octave"""
         return int(self.semi_from_c0 / 12)
 
     def get_symbol(self):
+        """Gets symbol"""
         return BASE_NOTES[self.semi_from_c0 % 12]
 
     def get_semi_from_a4(self):
+        """Get distance in semitones from A4"""
         return self.semi_from_c0 - _A4_BASE
 
     def increment(self, semitones):
+        """Increment note with this number of semitones"""
         self.semi_from_c0 += semitones
         return self
 
     def frequency(self):
-        return (2 ** (self.get_semi_from_a4()/12.)) * 440
+        """Calculate frequency of note"""
+        return (2 ** (self.get_semi_from_a4()/12.)) * _A4_TUNING
 
     @staticmethod
-    def calc_semi_from_c0(symbol, octave):
+    def calc_semi_from_c0(symbol: str, octave: int):
+        """Calculate semitones from c0
+
+        Args:
+            symbol: symbol of note (e.g. C, C#, etc., do not use flats)
+            octave: octave of note (see doc on top of module)
+        """
         return BASE_NOTES.index(symbol) + octave * 12
 
     def __repr__(self):
