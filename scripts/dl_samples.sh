f330f7020a083f064d01dc1fdbc8b6fa31ed1533
marcelraas <marcelraas@godatadriven.com>
2018-02-09 21:17:02 +0100

    Add notebook strip output attr

--- scripts/dl_samples.sh
+++ scripts/dl_samples.sh
@@ -4,3 +4,5 @@ youtube-dl -x --audio-format wav https://www.youtube.com/watch\?v\=fm7ntyycGbU
 youtube-dl -x --audio-format wav https://www.youtube.com/watch\?v\=JEKTNtmKZEw
 youtube-dl -x --audio-format wav https://www.youtube.com/watch\?v\=gNeqUxxPNVA
 youtube-dl -x --audio-format wav https://www.youtube.com/watch\?v\=RVKVT5z2Rwo
+
+mv *.wav ../data
