2d53c52180867a42ba65ecfd6c6335a48452d250
marcelraas <marcelraas@godatadriven.com>
2018-02-09 21:08:39 +0100

    Expand interface of play_array

--- music_generator/analysis/play.py
+++ music_generator/analysis/play.py
@@ -30,20 +30,34 @@ def play_mono_as_stereo(array: np.array,
 
 def play_array(array: np.array,
                sample_rate=44100,
-               norm=1.0):
+               norm=1.0,
+               do_wait_done=True,
+               range_secs=None):
     """Plays mono numpy array and waits until done
 
     Args:
         array (np.array): numpy array
         sample_rate (int): sample rate
-        norm (float): norm
+        norm (float): norm (defines max-volume level)
+        do_wait_done (bool): whether or not to wait until playback is complete
+        range_secs (None or float[2]): range in seconds to playback
 
     Returns:
         None
     """
+    if range_secs is not None:
+        sample_start = int(range_secs[0] * sample_rate)
+        sample_end = int(range_secs[1] * sample_rate)
+        sample_end = min(sample_end, len(array))
+
+        assert 0 <= sample_start < len(array)
+        assert 0 <= sample_end < len(array)
+        assert sample_start < sample_end
+        array = array[sample_start:sample_end]
 
     scale = 2**15 - 1
     array = ((array / norm).clip(-1, 1) * scale).astype(np.int16)
 
     po = play_buffer(array, 1, 2, sample_rate)
-    po.wait_done()
+    if do_wait_done:
+        po.wait_done()
