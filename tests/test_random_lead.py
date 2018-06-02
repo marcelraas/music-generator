eb7868a74a12056adcf4280ee13b9b991d34ed02
marcelraas <marcelraas@godatadriven.com>
2018-06-02 17:59:51 +0200

    Add test checks

--- tests/test_random_lead.py
+++ tests/test_random_lead.py
@@ -1,9 +1,15 @@
 from music_generator.basic import random_lead
 from music_generator.musical.scales import GenericScale
+from music_generator.musical.chords import Chord
 
 
 def test_generate_chords():
     scale = GenericScale('C', [0, 2, 3, 5, 7, 8, 10])
-    chord_track = random_lead.generate_chords(scale, 4, 4, 32)
+    chord_track = random_lead.generate_chords(scale, 32, 4, 4)
+
+    assert len(chord_track) == 32
+    assert type(chord_track[0]) == Chord
 
     pass
+
+
