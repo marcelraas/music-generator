d2bb893332e194e474619ed2aa28bd23103dfc73
marcelraas <marcelraas@godatadriven.com>
2019-04-05 16:51:14 +0200

    Remove requirements file

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 from setuptools import setup, find_packages
 
-requirements = []
+# requirements = ['torch==1.0.0',
+#                 'scikit-image==0.14.1',
+#                 'scikit-learn==0.20.1',
+#                 'pandas==0.23.4',
+#                 'matplotlib==3.0.2',
+#                 'plotnine==0.5.1',
+#                 'tqdm==4.28.1']
+
+requirements = [
+    'tensorflow>=2.0.0',
+    'numpy',
+    'pandas',
+    'matplotlib',
+    'scikit-learn',
+    'scipy'
+]
 
 setup(
     name='music_generator',
