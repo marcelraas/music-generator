d8fe7b127ffcebe751125bc88213e9442641e839
marcelraas <marcelraas@godatadriven.com>
2019-09-20 14:39:43 +0200

    Add jupyter dependency

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from setuptools import setup, find_packages
 
-# requirements = ['torch==1.0.0',
-#                 'scikit-image==0.14.1',
-#                 'scikit-learn==0.20.1',
-#                 'pandas==0.23.4',
-#                 'matplotlib==3.0.2',
-#                 'plotnine==0.5.1',
-#                 'tqdm==4.28.1']
-
 requirements = [
     'tensorflow>=2.0.0-rc1',
     'numpy',
